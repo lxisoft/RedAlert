5ee8aae4294e6c6675552ddf888100dcff18a79c
Anzal Khan <ansalkhan.k.a@lxisoft.com>
2018-11-08 09:29:44 +0530

    UI-Home and index pages

---
+++ RedAlert-UI/src/main/resources/static/js/custom.js
@@ -0,0 +1,62 @@
+// This code dosen't works on Firefox and IE and works on other browesers.
+$(document).ready(function () {
+$('.animated-icon1,.animated-icon3,.animated-icon4').click(function () {
+$(this).toggleClass('open');
+});
+});
+
+// Works everywhere
+$(document).ready(function () {
+
+// Hide/show animation hamburger function
+$('.navbar-toggler').on('click', function () {
+
+// Take this line to first hamburger animations
+$('.animated-icon1').toggleClass('open');
+
+// Take this line to second hamburger animation
+$('.animated-icon3').toggleClass('open');
+
+// Take this line to third hamburger animation
+$('.animated-icon4').toggleClass('open');
+});
+
+});
+document.addEventListener("DOMContentLoaded", init, false);
+	
+	function init() {
+		document.querySelector('#files').addEventListener('change', handleFileSelect, false);
+		
+	}
+		
+	function handleFileSelect(e) {
+		
+		if(!e.target.files) return;		
+		var files = e.target.files;
+		var fileslist=document.getElementById("fileslist");
+		for(var i=0; i<files.length; i++) {
+			var f = files[i];
+			if(fileslist.value!='')
+			{
+				fileslist.value=fileslist.value+', '+f.name;
+			}
+			else
+			{
+				fileslist.value=f.name;
+			}
+		}
+		
+	}
+
+function setModal(button) {
+	if(button.id=='red')
+	{
+		document.getElementById('modal-title').innerHTML='RedAlert';
+		document.getElementById('alertLevel').value="RED";
+	}
+	else
+	{		
+		document.getElementById('modal-title').innerHTML='OrangeAlert';
+		document.getElementById('alertLevel').value="ORANGE";
+	}
+}
