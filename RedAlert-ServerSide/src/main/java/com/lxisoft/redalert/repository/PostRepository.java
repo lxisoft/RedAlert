8486d4fc5b218aefe43049cfce1847c934d8e26b
sahalr1 <mohammed.sahal@lxisoft.com>
2018-11-28 12:28:49 +0530

    solving conflicts

--- RedAlert-ServerSide/src/main/java/com/lxisoft/redalert/repository/PostRepository.java
+++ RedAlert-ServerSide/src/main/java/com/lxisoft/redalert/repository/PostRepository.java
@@ -1,6 +1,14 @@
 package com.lxisoft.redalert.repository;
 
 import com.lxisoft.redalert.domain.Post;
+import com.lxisoft.redalert.domain.UserRegistration;
+import com.lxisoft.redalert.service.dto.ActionDTO;
+import com.lxisoft.redalert.service.dto.PostDTO;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +19,9 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+	Optional<Page<Post>> findAllByUserRegistration(Pageable pageable, UserRegistration userRegistration);
+
+	
 
 }
