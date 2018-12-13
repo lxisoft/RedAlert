c0899546755882d918278c02a895246f2fd36fdc
abhina <abhina.s@lxisoft.com>
2018-12-13 12:36:38 +0530

    security config

--- RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/config/SecurityConfiguration.java
+++ RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/config/SecurityConfiguration.java
@@ -8,10 +8,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -19,39 +26,42 @@ import org.springframework.web.filter.CorsFilter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 @Configuration
-@EnableOAuth2Sso
+/*@EnableOAuth2Sso*/
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Import(SecurityProblemSupport.class)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final CorsFilter corsFilter;
+   /* private final CorsFilter corsFilter;
 
     private final SecurityProblemSupport problemSupport;
-
-    public SecurityConfiguration(CorsFilter corsFilter, SecurityProblemSupport problemSupport) {
+*/
+   /* public SecurityConfiguration(CorsFilter corsFilter, SecurityProblemSupport problemSupport) {
         this.corsFilter = corsFilter;
         this.problemSupport = problemSupport;
     }
-
+*/
     @Bean
     public AjaxLogoutSuccessHandler ajaxLogoutSuccessHandler() {
         return new AjaxLogoutSuccessHandler();
     }
+    @Autowired
+    DataSource datasource;
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
+   /* @Override
+   public void configure(WebSecurity web) throws Exception {
         web.ignoring()
             .antMatchers(HttpMethod.OPTIONS, "/**")
-            .antMatchers("/app/**/*.{js,html}")
-            .antMatchers("/i18n/**")
-            .antMatchers("/content/**")
-            .antMatchers("/swagger-ui/index.html")
-            .antMatchers("/test/**");
-    }
+          //  .antMatchers("/app/**///*.{js,html}")
+        //    .antMatchers("/i18n/**")
+      //      .antMatchers("/content/**")
+    //        .antMatchers("/swagger-ui/index.html")
+  //          .antMatchers("/test/**");
+//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
+       /* http
             .csrf()
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         .and()
@@ -74,6 +84,46 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN);
+        
+        
+        
+        */
+        /*http
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/","/login").permitAll()
+       .anyRequest().authenticated()
+       .and().formLogin().loginPage("/index").defaultSuccessUrl("/test").permitAll();*/
+       
 
+    	
+    	http
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/redAlertUi/newsPage/**")
+        .hasAnyRole("USER")
+        .and()
+        .formLogin().loginPage("/loginController/login").permitAll();
+    	
+    }
+    
+    
+    
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+      throws Exception {
+       System.out.println("configuration abhina password=========================="+new BCryptPasswordEncoder().encode("abhina"));
+        
+       /* auth.inMemoryAuthentication()      
+        .withUser("abhina").password("{noop}pass1").roles("USER"); 
+      */
+        auth.jdbcAuthentication().dataSource(datasource).passwordEncoder(new BCryptPasswordEncoder()).
+	      
+	      usersByUsernameQuery("select id,password,activated from jhi_user where id=?")
+	      
+         .authoritiesByUsernameQuery("select user_id,authority_name from jhi_user_authority where user_id=?");
     }
 }
