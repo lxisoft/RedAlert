1dd23b906d18d52de4d17b5974df548e296f2098
Vineeth <silpa.s@lxisoft.com>
2018-12-15 15:45:20 +0530

    new viewController

--- RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/web/rest/controller/ViewController.java
+++ RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/web/rest/controller/ViewController.java
@@ -3,58 +3,56 @@ package com.lxisoft.redalert.web.rest.controller;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import java.util.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;   
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.codahale.metrics.annotation.Timed;
 import com.lxisoft.redalert.client.red_alert.api.MediaResourceApi;
 import com.lxisoft.redalert.client.red_alert.api.PostResourceApi;
 import com.lxisoft.redalert.client.red_alert.api.UserRegistrationResourceApi;
+import com.lxisoft.redalert.client.red_alert.model.ActionDTO;
 import com.lxisoft.redalert.client.red_alert.model.MediaDTO;
-import com.lxisoft.redalert.client.red_alert.model.PostDTO;
-import com.lxisoft.redalert.client.red_alert.model.PostDTO.AlertLevelEnum;
-import com.lxisoft.redalert.client.red_alert.model.UserRegistrationDTO;
+import com.lxisoft.redalert.client.red_alert.model.PostDTO;	
+import com.lxisoft.redalert.client.red_alert.model.PostDTO.AlertLevelEnum;	
+import com.lxisoft.redalert.client.red_alert.model.UserRegistrationDTO;	
+import com.lxisoft.redalert.model.ImageView;	
 import com.lxisoft.redalert.model.View;
-
+/**	
+ * @author Silpa	
+ *	
+ */
 @Controller
 @RequestMapping("/redAlertUi")
 public class ViewController {
-	@Autowired
-	PostResourceApi postResourceApi;
-	@Autowired
-	MediaResourceApi mediaResourceApi;
-	@Autowired
+	@Autowired	 	
+ 	PostResourceApi postResourceApi;	 
+ 	@Autowired	 	
+ 	MediaResourceApi mediaResourceApi;	 	
+	@Autowired		
 	UserRegistrationResourceApi userRegistrationResourceApi;
 	
-	@GetMapping("/index")
-	public String getIndex(Model model)
+	@GetMapping("/index")	
+	public String getIndex(Model model)		
 	{
-		View view = new View();
-		view.setUserRegistrationDTO(new UserRegistrationDTO());
-		model.addAttribute(view);
-		return "index";
+		View view = new View();	 		
+ 		view.setUserRegistrationDTO(new UserRegistrationDTO());	 		
+ 		model.addAttribute("view",view);	 
+ 		return "index";
 	}
 	
-
-
-
 	/**
 	 * @param model
 	 * @return
 	 */
-
 	@GetMapping("/home")
 	public String getHome(Model model)
 	{
@@ -68,6 +66,14 @@ public class ViewController {
 		return "home";
 	}
 	
+	/**
+	 * @param view
+	 * @param files
+	 * @param users
+	 * @param medias
+	 * @param userAlertLevel
+	 * @return
+	 */
 	@PostMapping("/getAlertDetailsToPost")
 	public String getAlertDetailsToPost(@ModelAttribute View view,@RequestParam("files") MultipartFile[] files,
 			@ModelAttribute UserRegistrationDTO users,
@@ -101,7 +107,24 @@ public class ViewController {
 		return "redirect:/redAlertUi/home";
 	}
 	
+	/**
+	 * @return
+	 */
+	@GetMapping("/news")
+	public String getNews(Model model)
+	{
+		View view = new View();
+		view.setPosts((ArrayList<PostDTO>) postResourceApi.getAllPostsUsingGET(null, null, null, null, null, null, null, null, null, null).getBody());
 
+		model.addAttribute("view",view);
+
+	   return "news";
+	}
+	
+	/**
+	 * @return
+	 */
+	
 	
 	@GetMapping("/friends")
 	public String getFriends()
@@ -113,107 +136,44 @@ public class ViewController {
 	public String getHistory(Model model)
 	{
 		View view = new View();
-		view.setPosts((ArrayList<PostDTO>) postResourceApi.getAllPostsUsingGET(null, null, null, null, null, null, null, null, null, null).getBody());
-		view.setMedias((ArrayList<MediaDTO>) mediaResourceApi.getAllMediaUsingGET(null, null, null, null, null, null, null, null, null, null).getBody());
-		view.setUsers((ArrayList<UserRegistrationDTO>) userRegistrationResourceApi.getAllUserRegistrationsUsingGET(null, null, null, null, null, null, null, null, null, null, null).getBody());
-		view.setPostDTO(view.getPosts().get(0));
-		view.setMediaDTO(view.getMedias().get(0));
-		System.out.println("Id "+view.getPostDTO().getDescription());
-		//System.out.println("name "+view.getUserRegistrationDTO().getFirstName());
+		ArrayList<ImageView> imageViews=new ArrayList<ImageView>();
 		
-		view.setUserRegistrationDTO(userRegistrationResourceApi.getUserRegistrationUsingGET(view.getPostDTO().getUserRegistrationId()).getBody());
+		ArrayList<PostDTO> posts = (ArrayList<PostDTO>) postResourceApi.getAllPostsByUserRegistrationIdUsingGET((long)1, null, null, null, null, null, null, null, null, null, null).getBody();
+
+		
+		for(PostDTO post:posts)
+		{
+			ImageView imageView=new ImageView(); 
+			
+		
+			ArrayList<MediaDTO> medias = (ArrayList<MediaDTO>) mediaResourceApi.getAllMediaByPostIdUsingGET(post.getId(), null, null, null, null, null,null, null, null, null, null).getBody();
+			ArrayList<String> images = new ArrayList<String>();
+			for(MediaDTO media:medias)
+			{
+				
+				String image="data:image/jpg;base64,"+Base64.getEncoder().encodeToString(media.getFile());
+				images.add(image);
+				imageView.setImages(images);
+				
+			}
+			
+			 imageView.setMedia(medias);
+			 imageView.setPost(post);
+			 imageViews.add(imageView);
+		}
+		  
+		
+		
+		
+		view.setImageViews(imageViews);
+		
+		view.setUserRegistrationDTO(userRegistrationResourceApi.getUserRegistrationUsingGET((long)1).getBody());
+	
 		model.addAttribute("view",view);
 	   return "history";
 	}
 	
 }
 	
-	/*@GetMapping("/getAlert")
-	public String getAction(@ModelAttribute PostDTO postDTO,Model model)
-	{
-		if ((postDTO.getAlertLevel().equals(PostDTO.AlertLevelEnum.ORANGE))) {
-			postDTO.setAlertLevel(PostDTO.AlertLevelEnum.ORANGE);
-			//postDTO = PostService.save(postDTO);
-			postResourceApi.updatePostUsingPUT(postDTO);
-			System.out.println("first feed " + postDTO);
-			View view = new View();
-
-			MediaDTO mediaDTO = new MediaDTO();
-			mediaDTO.setPostId(postDTO.getId());
-			view.setPostDTO(postDTO);
-			view.setMediaDTO(mediaDTO);
-			System.out.println("first file " + view.getPostDTO() + "*****" + view.getMediaDTO().getPostId());
-			model.addAttribute("view", view);
-
-			return "home";
-	}else if ((postDTO.getAlertLevel().equals(PostDTO.AlertLevelEnum.RED))) {
-		postDTO.setAlertLevel(PostDTO.AlertLevelEnum.RED);
-		//feed = feedService.save(feed);
-		postResourceApi.updatePostUsingPUT(postDTO);
-		System.out.println("first feed " + postDTO);
-		View view = new View();
-
-		MediaDTO mediaDTO = new MediaDTO();
-		mediaDTO.setPostId(postDTO.getId());
-		view.setPostDTO(postDTO);
-		view.setMediaDTO(mediaDTO);
-		System.out.println("first file " + view.getPostDTO() + "*****" + view.getMediaDTO().getPostId());
-		model.addAttribute("view", view);
-		return "home";
-	} else {
-		postDTO.setAlertLevel(PostDTO.AlertLevelEnum.GREEN);
-		//postDTO = feedService.save(feed);
-		postResourceApi.updatePostUsingPUT(postDTO);
-		System.out.println("first feed " + postDTO);
-		View view = new View();
-
-		MediaDTO mediaDTO = new MediaDTO();
-		mediaDTO.setPostId(postDTO.getId());
-		view.setPostDTO(postDTO);
-		view.setMediaDTO(mediaDTO);
-		System.out.println("first file " + view.getPostDTO() + "*****" + view.getMediaDTO().getPostId());
-		model.addAttribute("view", view);
-		return "home";
-	}
-	
-	}
-	@PostMapping("/getform")
-	@Timed
-	public String createFeed(@ModelAttribute View view, @RequestParam MultipartFile img,
-			RedirectAttributes redirectAttr, Model model) throws URISyntaxException {
-		System.out.println("second View Dto" + view);
-		System.out.println("second feed dto" + view.getPostDTO() + "id" + view.getMediaDTO());
-		
-		AlertLevelEnum alertLevel = view.getPostDTO().getAlertLevel();
-		System.out.println("testing..."+ alertLevel);
-		view.getPostDTO().setAlertLevel(alertLevel);;
-		//PostDTO postDto = postService.findOne(view.getPostDTO().getId());
-		 ResponseEntity<PostDTO> postDto = postResourceApi.getPostUsingGET(view.getPostDTO().getId());
-		 
-		postDto.getBody().setDescription(view.getPostDTO().getDescription());
-		postDto.getBody().setAlertLevel(view.getPostDTO().getAlertLevel());
-		System.out.println("feeddto nnnn" + postDto);
-
-		postResourceApi.updatePostUsingPUT(postDto.getBody());
-		ResponseEntity<MediaDTO> mediaDTO = mediaResourceApi.createMediaUsingPOST(view.getMediaDTO());
-		mediaDTO.getBody().setPostId(view.getPostDTO().getId());
-
-		try {
-			
-			String imagedata="data:image/jpg;base64,"+Base64.getEncoder().encodeToString(img.getBytes());
-			model.addAttribute("image", imagedata);
-			mediaDTO.getBody().setFile(img.getBytes());
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("second file dto" + view.getMediaDTO().getFile());
-		//fileservice.save(view.getFile());
-		mediaResourceApi.updateMediaUsingPUT(view.getMediaDTO());
-		System.out.println("successsful");
-		return "home";
-	}*/
 
 
