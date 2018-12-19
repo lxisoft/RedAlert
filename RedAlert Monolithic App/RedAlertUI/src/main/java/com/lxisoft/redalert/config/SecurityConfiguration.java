fe8338ad9171ae9a590dde81d3ee148222edf3e6
Vineeth <silpa.s@lxisoft.com>
2018-12-19 11:55:45 +0530

    liuibase change

--- RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/config/SecurityConfiguration.java
+++ RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/config/SecurityConfiguration.java
@@ -107,7 +107,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .csrf()
         .disable()
         .authorizeRequests()
-        .antMatchers("/redAlertUi/**")
+        .antMatchers("/khhkj/**")
         .hasAnyRole("USER")
         .and()
         .formLogin().loginPage("/loginController/login").permitAll();
