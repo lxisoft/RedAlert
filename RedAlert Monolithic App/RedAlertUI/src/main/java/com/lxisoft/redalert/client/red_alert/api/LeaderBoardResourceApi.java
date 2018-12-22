e3364fcb1c37361849339f73644befa521a4ed20
imanzal <ansalkhan.k.a@lxisoft.com>
2018-12-22 21:03:31 +0530

    Add user to friendlist

--- RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/client/red_alert/api/LeaderBoardResourceApi.java
+++ RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/client/red_alert/api/LeaderBoardResourceApi.java
@@ -28,7 +28,7 @@ import java.util.Optional;
 
 
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2018-12-21T14:04:22.479054500+05:30[Asia/Calcutta]")
+
 
 
 @Api(value = "LeaderBoardResource", description = "the LeaderBoardResource API")
