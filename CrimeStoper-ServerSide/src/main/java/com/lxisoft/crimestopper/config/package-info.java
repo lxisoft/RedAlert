10e43db1598d3d3cfcdb8d7f39b55ba078f57ba6
SoOrAjPn <sooraj.p.n@lxisoft.com>
2019-01-04 15:19:08 +0530

    remove crimestopper

--- CrimeStoper-ServerSide/src/main/java/com/lxisoft/crimestopper/config/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Framework configuration files.
- */
-package com.lxisoft.crimestopper.config;
--- CrimeStoper-ServerSide/src/main/java/com/lxisoft/crimestopper/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.lxisoft.crimestopper.service.dto;
