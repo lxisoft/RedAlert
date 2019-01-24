0f44a9eb773ec0bb44ce39859972b438ac4dab68
imanzal <ansalkhan.k.a@lxisoft.com>
2019-01-24 12:10:58 +0530

    Debugged

--- RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/client/red_alert/model/PostDTO.java
+++ RedAlert Monolithic App/RedAlertUI/src/main/java/com/lxisoft/redalert/client/red_alert/model/PostDTO.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
@@ -58,7 +60,7 @@ public class PostDTO   {
   private AlertLevelEnum alertLevel = null;
 
   @JsonProperty("createdOn")
-  private OffsetDateTime createdOn = null;
+  private Instant createdOn = null;
 
   @JsonProperty("description")
   private String description = null;
@@ -115,7 +117,7 @@ public class PostDTO   {
     this.alertLevel = alertLevel;
   }
 
-  public PostDTO createdOn(OffsetDateTime createdOn) {
+  public PostDTO createdOn(Instant createdOn) {
     this.createdOn = createdOn;
     return this;
   }
@@ -128,12 +130,12 @@ public class PostDTO   {
 
   @Valid
 
-  public OffsetDateTime getCreatedOn() {
+  public Instant getCreatedOn() {
     return createdOn;
   }
 
-  public void setCreatedOn(OffsetDateTime createdOn) {
-    this.createdOn = createdOn;
+  public void setCreatedOn(Instant instant) {
+    this.createdOn = instant;
   }
 
   public PostDTO description(String description) {
